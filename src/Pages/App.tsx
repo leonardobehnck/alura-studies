5906f3fdba866c01007f2ee1e15a798249317eb3
Leonardo Behnck <leonardo.behnck@gmail.com>
2022-09-01 17:48:35 -0300

    Update App.tsx

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -9,6 +9,16 @@ import { ITarefa } from "../types/tarefa";
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([])
+  const [selecionado, setSelecionado] = useState<ITarefa>()
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada)
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(_tarefa =>{
+      ...tarefas, 
+      selecionado: tarefaSelecionada.id === tarefaSelecionada.id ? true : false
+    }))
+  }
+
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
